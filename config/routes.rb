33da3f451d07ebe6257798cd79e92ad4573dc374
hgkaa <107188438+hgkaa@users.noreply.github.com>
2022-08-13 23:20:40 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'homes/top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'books#top'
+
+  resources :books
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'homes/top' => 'homes#top'
 end
